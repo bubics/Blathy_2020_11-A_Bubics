bce5561843e493e0aaf9746dfbba24cbe28be745
Bubee <bubicsbence@gmail.com>
2021-03-18 17:13:41 +0100

    command,list update

--- film_ertekeles/film_ertekeles/MainVM.cs
+++ film_ertekeles/film_ertekeles/MainVM.cs
@@ -1,10 +1,13 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace film_ertekeles
 {
@@ -13,6 +16,8 @@ namespace film_ertekeles
         public MainVM()
         {
             this.PropertyChanged += MainVM_PropertyChanged;
+            Elemek = new ObservableCollection<string>();
+            Add = new RelayCommand(Add_action);
         }
 
         private void MainVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -32,5 +37,16 @@ namespace film_ertekeles
 
         public string SearchWord { get => searchWord; set => Set(ref searchWord, value); }
         public int Db { get => db; set => Set(ref db, value); }
+
+        public ICommand Add { get; set; }
+        private void Add_action()
+        {
+            if (!string.IsNullOrWhiteSpace(SearchWord))
+            {
+                Elemek.Add(SearchWord);
+                SearchWord = "";
+            }
+        }
+        public ObservableCollection<string> Elemek { get; private set; }
     }
 }
