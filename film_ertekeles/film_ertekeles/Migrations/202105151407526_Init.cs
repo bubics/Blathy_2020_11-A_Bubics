ba604a27a05bf5f75d68cbdf308653a38fac12e1
Bubee <bubicsbence@gmail.com>
2021-05-24 10:31:55 +0200

    database_update

---
+++ film_ertekeles/film_ertekeles/Migrations/202105151407526_Init.cs
@@ -0,0 +1,26 @@
+﻿namespace film_ertekeles.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.filmeks",
+                c => new
+                    {
+                        film = c.String(nullable: false, maxLength: 128),
+                        ertekeles = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.film);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.filmeks");
+        }
+    }
+}
---
+++ film_ertekeles/film_ertekeles/Model/filmek.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace film_ertekeles.Model
+{
+    public class filmek : ObservableObject
+    {
+        [Key]
+        public string Filma { get => filma; set => Set(ref filma, value); }
+        [Required]
+        public int Ertekelesa { get => ertekelesa; set => Set(ref ertekelesa, value); }
+
+        private string filma;
+        private int ertekelesa;
+    }
+}
